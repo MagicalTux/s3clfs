c7ca9290ae50cbe0740662349826be4d49a2f3d2
Mark Karpeles <magicaltux@gmail.com>
2015-02-10 06:11:33 +0900

    set cache location?

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -1,5 +1,6 @@
 #include <QCoreApplication>
 #include <QCommandLineParser>
+#include <QStandardPaths>
 #include <S3FS.hpp>
 
 int main(int argc, char *argv[]) {
@@ -16,6 +17,7 @@ int main(int argc, char *argv[]) {
 	parser.addPositionalArgument("path", QCoreApplication::translate("main", "Path where to mount file system"));
 	parser.addOption({{"o", "options"}, QCoreApplication::translate("main", "comma-separated list of mount options"), "options"}); // if compilation fails here, make sure you have Qt 5.4+
 	parser.addOption({{"q", "queue"}, QCoreApplication::translate("main", "URL of SQS queue holding events for this bucket"), "queue"});
+	parser.addOption({{"c", "cache"}, QCoreApplication::translate("main", "Where to store cache, default %1/<bucket>").arg(QStandardPaths::writableLocation(QStandardPaths::AppDataLocation)), "cache"});
 
 	parser.process(app);
 
